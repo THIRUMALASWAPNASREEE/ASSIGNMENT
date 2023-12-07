aac9bb497416485efdc18c0a255af814cb28f044
THIRUMALASWAPNASREEE <153164860+THIRUMALASWAPNASREEE@users.noreply.github.com>
2023-12-07 11:59:51 +0530

    EASY1.py

--- EASY1.py
+++ EASY1.py
@@ -1,6 +1,4 @@
-Given a string s consisting of words and spaces, return the length of the last word in the string.
-A word is a maximal 
-substring consisting of non-space characters only.
+#Given a string s consisting of words and spaces, return the length of the last word in the string.A word is a maximal substring consisting of non-space characters only.
 def length_of_last_word(s):
     words = s.split()
     if not words:
